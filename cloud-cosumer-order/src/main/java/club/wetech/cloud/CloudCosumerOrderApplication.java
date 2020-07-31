43fefd774eaee939876363d790a10829364d4b6f
leven <904150336@qq.com>
2020-07-31 18:09:25 +0800

    配置子module pom版本

---
+++ cloud-cosumer-order/src/main/java/club/wetech/cloud/CloudCosumerOrderApplication.java
@@ -0,0 +1,15 @@
+package club.wetech.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leven.chen
+ */
+@SpringBootApplication
+public class CloudCosumerOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudCosumerOrderApplication.class, args);
+    }
+}
